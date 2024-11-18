138008d5223944fa6bb8987dc24ff7dbf93bf858
Sachin Kumar <sachindevpanwar@gmail.com>
2024-11-19 01:56:50 +0530

    Setup in windows / database connectivity done

--- ShopNBuyWebParent/ShopNBuyBackEnd/src/main/java/com/shopnbuy/admin/ShopNBuyBackEndApplication.java
+++ ShopNBuyWebParent/ShopNBuyBackEnd/src/main/java/com/shopnbuy/admin/ShopNBuyBackEndApplication.java
@@ -3,14 +3,11 @@ package com.shopnbuy.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-@EntityScan({ "com.shopnbuy.common.entity", "com.shopnbuy.admin.user" })
+@SpringBootApplication
+@EntityScan(basePackages = { "com.shopnbuy.common.entity", "com.shopnbuy.admin.user", "com.common.entity" })
 public class ShopNBuyBackEndApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShopNBuyBackEndApplication.class, args);
 	}
-
 }
