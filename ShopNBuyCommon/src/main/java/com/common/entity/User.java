a7c83cf426dd6f926b57b5770dadd4773adbc37c
Sachin Kumar <sachindevpanwar@gmail.com>
2024-11-24 20:39:40 +0530

    new user page completed

--- ShopNBuyCommon/src/main/java/com/common/entity/User.java
+++ ShopNBuyCommon/src/main/java/com/common/entity/User.java
@@ -1,6 +1,5 @@
 package com.common.entity;
 
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,96 +23,106 @@ public class User {
 	private String email;
 	@Column(length = 64, nullable = false)
 	private String password;
-	
-	@Column(name = "first_name",length = 45, nullable = false)
+
+	@Column(name = "first_name", length = 45, nullable = false)
 	private String firstName;
-	@Column(name = "last_name",length = 45, nullable = false)
+	@Column(name = "last_name", length = 45, nullable = false)
 	private String lastName;
-	
+
 	@Column(length = 64)
 	private String photos;
 	private boolean enabled;
-	
-	/* need to map a many to many relationship between role & user
-	 * jointable is used for many to many, connect the primary keys of 
-	 * the two tables, in this case the ids for user & role
- 	 * 
-	 * */
-	
-	@ManyToMany
-	@JoinTable(
-			name = "users_roles",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id")
-			)
-	private Set<Role> roles = new HashSet<>();
 
-	
+	/*
+	 * need to map a many to many relationship between role & user
+	 * jointable is used for many to many, connect the primary keys of
+	 * the two tables, in this case the ids for user & role
+	 * 
+	 */
+
+	@ManyToMany
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
-	public void setFirstname(String firstName) {
+
+	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getPhotos() {
 		return photos;
 	}
+
 	public void setPhotos(String photos) {
 		this.photos = photos;
 	}
+
 	public boolean isEnabled() {
 		return enabled;
 	}
+
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
-	
+
 	public void addRole(Role role) {
-		
+
 		this.roles.add(role);
 	}
-	
+
 	public User() {
-		
+
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName
 				+ ", roles=" + roles + "]";
 	}
+
 	public User(String email, String password, String firstName, String lastName) {
 		super();
 		this.email = email;
--- ShopNBuyWebParent/ShopNBuyBackEnd/src/main/java/com/shopnbuy/admin/user/UserController.java
+++ ShopNBuyWebParent/ShopNBuyBackEnd/src/main/java/com/shopnbuy/admin/user/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.common.entity.Role;
 import com.common.entity.User;
 
 @Controller
@@ -24,4 +27,30 @@ public class UserController {
 
 		return "users.html";
 	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+
+		List<Role> listRoles = service.listRoles();
+
+		User user = new User();
+		user.setEnabled(true);
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+
+		return "user_from";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+
+		System.out.println(user);
+
+		service.save(user);
+
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
+
+		return "redirect:/users";
+	}
+
 }
